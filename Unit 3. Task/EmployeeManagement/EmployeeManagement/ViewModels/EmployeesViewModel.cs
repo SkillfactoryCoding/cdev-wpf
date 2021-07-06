7f9bdee851fc3467ace7cda1ef7c19dcc60b77ec
GitBrainsStudio <65179794+GitBrainsStudio@users.noreply.github.com>
2021-07-06 18:37:43 +0500

    Add files via upload

---
+++ Unit 3. Task/EmployeeManagement/EmployeeManagement/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    class EmployeesViewModel
+    {
+        private EmployeeRepository _employeeRepository { get; }
+        public EmployeesViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public ObservableCollection<Employee> Employees { get {
+                return new ObservableCollection<Employee>
+                    (this._employeeRepository.GetAll());} }
+
+    }
+}
---
+++ Unit 4. Task/EmployeeManagement/EmployeeManagement/Views/EmployeesView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeeManagement.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeesView.xaml
+    /// </summary>
+    public partial class EmployeesView : Window
+    {
+        public EmployeesView()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+
+            if (item is null)
+            {
+                return;
+            }
+
+            var employee = item as Employee;
+
+            MessageBox.Show(Message(employee));
+        }
+
+        private string Message(Employee employee)
+        {
+            return "Имя: " + employee.FirstName + "\n" +
+                "Фамилия: " + employee.LastName + "\n" +
+                "Возраст: " + employee.Age + "\n" +
+                "Должность: " + employee.Position + "\n" +
+                "Компания: " + employee.CompanyName + "\n" +
+                "Город: " + employee.CityName;
+        }
+    }
+}
